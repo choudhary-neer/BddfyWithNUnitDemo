c837d4d4a2459d19cd5f3f3cac459f16669f56e6
choudhary-neer <choudhary_neer@hotmail.com>
2021-01-03 18:20:19 +0530

    Add project files.

---
+++ TestWithFluentApi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TestStack.BDDfy;
+
+namespace NUnitTestProject1
+{
+    //http://pascallaurin42.blogspot.com/2014/07/exploring-bddfy.html
+    public class TestWithFluentApi
+    {
+        [TestCase]
+        public void ReusingStepForScenario1()
+        {
+            new TestWithFluentApi()
+                .Given(s => s.GivenWhatever(), "Given some pre-condition")
+                    .And(s => s.AndOtherGiven(54))
+                .When(s => s.WhenSomethingElseHappens())
+                .Then(s => s.ThenProfit())
+                    .And(s => s.AndManyMore(45))
+                .BDDfy();
+        }
+
+        [TestCase]
+        public void ReusingStepForScenario2()
+        {
+            new TestWithFluentApi()
+                .Given(s => s.GivenWhatever(), "Given some pre-condition")
+                    .And(s => s.AndOtherGiven(123))
+                .When(s => s.WhenSomethingElseHappens())
+                .Then(s => s.ThenProfit())
+                    .And(s => s.AndManyMore(321), "And {0} more things!")
+                .BDDfy("Scenario 2 with steps re-use");
+        }
+
+        void GivenWhatever()
+        { // ... 
+        }
+        void AndOtherGiven(int input)
+        { // ...
+        }
+        void WhenSomethingElseHappens()
+        { // ...
+        }
+        void ThenProfit()
+        { // ... 
+        }
+        void AndManyMore(int expected)
+        { // ... 
+        }
+    }
+}
---
+++ TestWithStoryAndScenarioInSeparatedClasses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TestStack.BDDfy;
+
+namespace NUnitTestProject1
+{
+    //http://pascallaurin42.blogspot.com/2014/07/exploring-bddfy.html
+
+    [Story(
+    Title = "Using story attribute and setting the Title!",
+    AsA = "As someone learning BDDfy",
+    IWant = "I want to try splitting scenario in separated classes",
+    SoThat = "My code is cleaner")]
+    public class TestWithStoryAndScenarioInSeparatedClasses
+    {
+        [TestCase]
+        public void FirstScenario()
+        {
+            new S1().BDDfy("Custom scenario title");
+        }
+
+        [TestCase]
+        public void SecondScenario()
+        {
+            new S2().BDDfy();
+        }
+
+        private class S1
+        {
+            void GivenWhatever()
+            {
+                // ... 
+            }
+            void WhenSomethingHappens()
+            {
+                // ... 
+            }
+            void ThenProfit()
+            {
+                // ... 
+            }
+        }
+        
+        private class S2
+        {
+            void GivenWhatever()
+            { // ... 
+            }
+            void WhenSomethingElseHappens()
+            { // ... 
+            }
+            void ThenProfit()
+            { // ... 
+            }
+        }
+    }
+}
